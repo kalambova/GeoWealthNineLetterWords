9fc013e3a0997a2808b8927df12d84e4183ab455
kalambova <nori.kalambova@gmail.com>
2025-04-16 20:10:18 +0300

    Adding solution for reducible 9 letters english words

---
+++ src/Main.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Main {
+    private static Set<String> allEnglishWordsDictionary = new HashSet<>();
+    private static final List<String> results = new ArrayList<>();
+
+    private static Set<String> loadAllWordsSet() throws IOException {
+
+        URL wordsURL = new URL("https://raw.githubusercontent.com/nikiiv/JavaCodingTestOne/master/scrabble-words.txt");
+        try (BufferedReader br = new BufferedReader(new
+                InputStreamReader(wordsURL.openConnection().getInputStream()))) {
+            return br.lines().skip(2).collect(Collectors.toSet());
+
+        }
+    }
+
+    private static boolean canReduce(String word) {
+        if (word.length() == 1) {
+            return word.equals("A") || word.equals("I");
+        }
+
+        for (int i = 0; i < word.length(); i++) {
+            String reduced = word.substring(0, i) + word.substring(i + 1);
+            if (allEnglishWordsDictionary.contains(reduced) && canReduce(reduced)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        allEnglishWordsDictionary = loadAllWordsSet();
+        allEnglishWordsDictionary.add("A");
+        allEnglishWordsDictionary.add("I");
+
+        Set<String> allNineLettersWords = loadAllWordsSet().stream()
+                .filter(s -> s.length() == 9)
+                .filter(s -> s.contains("A") || s.contains("I"))
+                .collect(Collectors.toSet());
+        System.out.println("Count of All 9 letter words:" + allNineLettersWords.size());
+
+        long start = System.currentTimeMillis();
+
+        for (String word : allNineLettersWords) {
+
+            if (canReduce(word)) {
+                results.add(word);
+            }
+        }
+
+        long finish = System.currentTimeMillis();
+        long timeElapsed = finish - start;
+        System.out.printf("Time elapsed: %d milliseconds\n", timeElapsed);
+
+        System.out.printf("Count of all reducible 9 letter words: %d\n", results.size());
+        System.out.printf("All reducible 9 letter words: %s\n", results);
+
+    }
+
+}
